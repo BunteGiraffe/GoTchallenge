e6d010c86e727c5f611601c149dfe51e607ac7df
Zoryana Paliy <z.paliy@gingco.net>
2021-06-04 13:43:12 +0200

    Clean up

--- projects/got-challenge/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of got-challenge
- */
-
-export * from './lib/got-challenge.service';
-export * from './lib/got-challenge.component';
-export * from './lib/got-challenge.module';
